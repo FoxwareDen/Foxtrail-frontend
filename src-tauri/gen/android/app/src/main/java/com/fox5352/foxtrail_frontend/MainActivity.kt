fa8f5ebc6c36c58232de0402aa78a1c04aa11163
fox5352 <christophervos522@gmail.com>
2025-09-05 18:11:50 +0200

    added backend section of the app

---
+++ src-tauri/gen/android/app/src/main/java/com/fox5352/foxtrail_frontend/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fox5352.foxtrail_frontend
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
