141767fab45152524b220d8dde3e0b67a84431e9
fox5352 <christophervos522@gmail.com>
2025-10-01 23:57:25 +0200

    added functions to manage sessions

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,15 +1,15 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, Session } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
-if (!supabaseUrl || !supabaseAnonKey){
-    throw new Error( 'Missing supabase env variables')
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing supabase env variables')
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export interface Job{
+export interface Job {
     id: number,
     created_at: string,
     api_id: string | null,
@@ -22,6 +22,30 @@ export interface Job{
     redirect_url: string | null,
     contract: string | null,
     company: string | null
+}
+
+export async function getSessionData(): Promise<Session | null> {
+    const { data, error } = await supabase.auth.getSession();
+
+    if (error) {
+        console.log(error)
+        return null
+    }
 
 
+    return data.session
+}
+
+export async function setSessionData(accessToken: string, refreshToken: string): Promise<Session | null> {
+    const { data, error } = await supabase.auth.setSession({
+        access_token: accessToken, refresh_token: refreshToken
+    })
+
+    if (error) {
+        console.log(error)
+        return null
+    }
+
+
+    return data.session
 }
