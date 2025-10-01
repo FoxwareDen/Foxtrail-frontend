2c73af92cb701aa1ef25bc91ba0aee37565f70e3
Christopher Vos <christophervos522@gmail.com>
2025-10-01 18:22:41 +0200

    moved runtime back to the lib.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,56 +1,50 @@
-use tauri_plugin_barcode_scanner;
 
-#[tauri::command]
-async fn scan<R: tauri::Runtime>(app_handle: tauri::AppHandle<R>) -> Option<String> {
-    None
-}
-
-// src-tauri/src/main.rs
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+// #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_barcode_scanner::init())
-        .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
+    foxtrail_lib::run();
+    // tauri::Builder::default()
+    //     .plugin(tauri_plugin_barcode_scanner::init())
+    //     .plugin(tauri_plugin_opener::init())
+    //     .plugin(tauri_plugin_os::init())
+    //     .plugin(tauri_plugin_shell::init())
+    //     .setup(|app| {
 
-            // Handle deep links
-            #[cfg(target_os = "android")]
-            {
-                let handle = app.handle().clone();
-                app.listen("android-intent", move |event| {
-                    if let Some(payload) = event.payload() {
-                        if let Ok(intent_data) = serde_json::from_str::<serde_json::Value>(payload)
-                        {
-                            if let Some(url) = intent_data.get("url").and_then(|u| u.as_str()) {
-                                if url.starts_with("com.fox5352.foxtrail://") {
-                                    let _ = handle.emit("deep-link", url);
-                                }
-                            }
-                        }
-                    }
-                });
-            }
+    //         // Handle deep links
+    //         #[cfg(target_os = "android")]
+    //         {
+    //             let handle = app.handle().clone();
+    //             app.listen("android-intent", move |event| {
+    //                 if let Some(payload) = event.payload() {
+    //                     if let Ok(intent_data) = serde_json::from_str::<serde_json::Value>(payload)
+    //                     {
+    //                         if let Some(url) = intent_data.get("url").and_then(|u| u.as_str()) {
+    //                             if url.starts_with("com.fox5352.foxtrail://") {
+    //                                 let _ = handle.emit("deep-link", url);
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             });
+    //         }
 
-            #[cfg(target_os = "ios")]
-            {
-                let handle = app.handle().clone();
-                app.listen("ios-url", move |event| {
-                    if let Some(payload) = event.payload() {
-                        if let Ok(url_data) = serde_json::from_str::<serde_json::Value>(payload) {
-                            if let Some(url) = url_data.get("url").and_then(|u| u.as_str()) {
-                                if url.starts_with("com.fox5352.foxtrail://") {
-                                    let _ = handle.emit("deep-link", url);
-                                }
-                            }
-                        }
-                    }
-                });
-            }
+    //         #[cfg(target_os = "ios")]
+    //         {
+    //             let handle = app.handle().clone();
+    //             app.listen("ios-url", move |event| {
+    //                 if let Some(payload) = event.payload() {
+    //                     if let Ok(url_data) = serde_json::from_str::<serde_json::Value>(payload) {
+    //                         if let Some(url) = url_data.get("url").and_then(|u| u.as_str()) {
+    //                             if url.starts_with("com.fox5352.foxtrail://") {
+    //                                 let _ = handle.emit("deep-link", url);
+    //                             }
+    //                         }
+    //                     }    
+    //                 }
+    //             });
+    //         }
 
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    //         Ok(())
+    //     })
+    //     .run(tauri::generate_context!())
+    //     .expect("error while running tauri application");
 }
